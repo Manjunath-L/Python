e38766474686b2c33c20b21615187c5097e0e9a7
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2025-05-29 23:10:58 +0530

    Day_2

---
+++ Day1.py
@@ -0,0 +1,6 @@
+Name = input("Name to print on certificate : ")
+Ach = input("Completed course : ")
+
+print("<--- Certificate of Achievement --->\n")
+print("This award is proudly presented to:\n" + Name + "\n")
+print("For: Completing "+ Ach + "Basics!")
