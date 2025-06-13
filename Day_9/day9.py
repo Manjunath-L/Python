186564ed09511401cd84a2d5b15c8c90dcdfcc74
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2025-06-13 23:07:51 +0530

    add day 9

---
+++ Day_9/day9.py
@@ -0,0 +1,28 @@
+from art import logo
+print(logo)
+bids = {}
+def finde_highest_bidder(bidding_dect):
+    winner = ""
+    highest_bid = 0
+    for bidder in bidding_dect:
+        bid_amount = bidding_dect[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bid_amount
+
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+
+while True:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    shude_continue = input("Are there any oter bidder? Type 'yes' or 'no'.\n").lower()
+    if shude_continue == "no":
+        finde_highest_bidder(bids)
+        break
+    elif shude_continue == "yes":
+        print("\n" * 20)
+
+
+
