b8b924e2208f567e3fef2d53152a1f132cb2a2dc
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2025-10-19 23:56:38 +0530

    first commit

---
+++ Day_11/black_jack.py
@@ -0,0 +1,44 @@
+import random
+
+
+def deal_card():
+    """Return a random card form the deck"""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+
+def calculate_score(cards):
+    """Takes a list of cards and return the score calculated form the card"""
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
+
+user_cards = []
+computer_cards = []
+is_game_over = False
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+while not is_game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    print(f"your cards : {user_cards} , current score :{user_score}")
+    print(f"computer first card : {computer_cards[0]}")
+
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        is_game_over = True
+    else:
+        user_should_card = input(
+            "Type 'Y' to get another card, type 'n' to pass: "
+        ).lower()
+        if user_should_card == "y":
+            user_cards.append(deal_card())
+        else:
+            is_game_over = True
